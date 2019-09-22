cca2729eac9bef347dbfda242b0b54267b05ba3b
Carrie Schmid <carriepederson7@gmail.com>
2019-09-22 10:59:07 -0700

    Add function to filter search for doctor names.

--- src/main.js
+++ src/main.js
@@ -4,61 +4,65 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
 
-
-
-
 $(document).ready(function() {
-  $("#health-info").submit(function(event) {
-    event.preventDefault();
-    const name = $("#doctor-name").val();
-    console.log(name);
-    const practice = $("#practice").val();
+      $("#health-info").submit(function(event) {
+          event.preventDefault();
+          const practice = $("#practice").val();
+          let docFirstNames = [];
+          console.log(docFirstNames);
+          let docLastNames = [];
+          console.log(docLastNames);
 
-    medInfo(name, practice);
+
+          $("#doc-search-button").click(function docSearch(){
+            const firstName = $("#doctor-first-name").val();
+            const lastName = $("#doctor-last-name").val();
+            console.log(firstName);
+            docFirstNames.forEach(function(docFirstName){
+              if(docFirstName === firstName){
+                $("#doctorSearch").text("One doctor matches your search.")
+              }
+
+            });
+            docLastNames.forEach(function(docLastName){
+              if(docLastName === lastName){
+                $("#doctorSearch").text("One doctor matches your search.")
+              }
+            })
+
+          })
 
 
-    function medInfo() {
 
-      let info = new Info();
-      let promise = info.conditions()
-      // let output = "";
-      promise.then(function(response) {
-        const body = JSON.parse(response);
-        console.log(body);
-        let profiles = body.data
-        profiles.forEach(function(profile) {
-          for (let i = 0; i < profile.specialties.length; i++) {
-            if (profile.specialties[i].actor === practice) {
-              $("#practiceSearch").append(`<li>${profile.profile.first_name} ${profile.profile.last_name}</li>`);
-              $("#practiceSearch").append(`${profile.practices[0].visit_address.street} , ${profile.practices[0].visit_address.city}, ${profile.practices[0].visit_address.state}, ${profile.practices[0].visit_address.zip},
+
+          let info = new Info();
+          let promise = info.conditions(name, practice)
+
+          promise.then(function(response) {
+              const body = JSON.parse(response);
+              console.log(body);
+              let profiles = body.data
+              profiles.forEach(function(profile) {
+                for (let i = 0; i < profile.specialties.length; i++) {
+                  if (profile.specialties[i].actor === practice) {
+                    $("#practiceSearch").append(`<li>${profile.profile.first_name} ${profile.profile.last_name}</li>`);
+                    docFirstNames.push(`${profile.profile.first_name}`); docLastNames.push(`${profile.profile.last_name}`);
+                    $("#practiceSearch").append(`${profile.practices[0].visit_address.street} , ${profile.practices[0].visit_address.city}, ${profile.practices[0].visit_address.state}, ${profile.practices[0].visit_address.zip},
                 ${profile.practices[0].phones[0].number} <br>Accepts new patients? ${profile.practices[0].accepts_new_patients}`);
-            }
-            // }else{
-            //   $("#practiceSearch").text("Sorry, no doctors match your search.");
-            // }
+                  }
+                }
+              });
 
-          }
-        });
-
-        profiles.forEach(function(profile) {
-          if (profile.profile.first_name === name || profile.profile.last_name === name) {
-            $("#doctorSearch").append(`<li>${profile.profile.first_name} ${profile.profile.last_name}</li>`);
-            $("#doctorSearch").append(`${profile.practices[0].visit_address.street} , ${profile.practices[0].visit_address.city}, ${profile.practices[0].visit_address.state}, ${profile.practices[0].visit_address.zip}, ${profile.practices[0].phones[0].number} <br>Accepts new patients? ${profile.practices[0].accepts_new_patients}`);
-          }
-
-          //   else{
-          //     let output =$("#doctorSearch").text("Sorry, no doctors match your search.");
-          //     return output[0]
-          //
-          // }
-        });
+            });
 
 
+              // docNames.forEach(function(docName) {
+              //   if (docName === name) {
+              //     alert("namefound");
+              //     // $("#doctorSearch").append(`${profile.profile.first_name} ${profile.profile.last_name}`)
+              //   }
+              // });
 
+
+          });
       });
-
-    }
-
-
-  });
-});
--- src/project.js
+++ src/project.js
@@ -3,7 +3,13 @@ export class Info {
   conditions() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      
+      const url =`https://api.betterdoctor.com/2016-03-01/doctors?&location=45.5051%2C-122.6750%2C100&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=${process.env.exports.apiKey}`
+
+      // const url =`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&specialty_uid=${practice}&location=45.5051%2C-122.6750%2C100&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=${process.env.exports.apiKey}`
+
+      // const url =`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5051%2C-122.6750%2C100&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=${process.env.exports.apiKey}`
+
+
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
